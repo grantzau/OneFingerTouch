1747a2a59fda8637879dc4859f3245af1010ac4e
Julius Graakjaer Grantzau <julius@grantzau.com>
2012-01-02 16:51:32 +0100

    renamed demo

--- demo/start/js/script.js
+++
@@ -1,4 +0,0 @@
-var touchEl = document.getElementById('touch');
-var touchTracker = new OneFingerTouch(touchEl);
-
-touchEl.addEventListener('onefingertouch-start', function(e){ console.log(e.onefingertouch); });
